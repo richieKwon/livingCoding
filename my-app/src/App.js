94793ad4750fcad07ee7951d8fe34f9d5d457436
richie <richie.kwon1@gmail.com>
2022-10-17 19:47:01 +0100

    component1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header(props) {
+  return <header>
+    <h1><a href='/'>{props.title}</a></h1>
+  </header>
+}
+
+function Nav(props) {
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html' },
+    { id: 2, title: 'css', body: 'css' },
+    { id: 3, title: 'javascript', body: 'javascript' }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
+      <Header title="React"></Header>
+      <Nav topics={topics}></Nav>
     </div>
+
   );
 }
 
